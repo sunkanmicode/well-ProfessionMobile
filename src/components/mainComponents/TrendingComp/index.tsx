a7b51af321adbea98f0d0a60e02a9f95c2e97fd9
Sunkanmi <sunkanmi.oguntimehin@optisoft.ng>
2023-08-11 21:45:00 +0100

    complete trending screen

--- src/components/mainComponents/TrendingComp/index.tsx
+++ src/components/mainComponents/TrendingComp/index.tsx
@@ -1,13 +1,12 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image, FlatList } from "react-native";
 import React from 'react'
-import { MaterialIcons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { useNavigation  } from "@react-navigation/native";
 
 
 
 
-const TrendingComp = () => {
+const TrendingComp = ({ trendingData }) => {
   const navigation = useNavigation();
 
   return (
@@ -28,53 +27,63 @@ const TrendingComp = () => {
       </View>
       <View className="pt-5">
         <Text className="font-[PlusBold] text-[18px]">Midstream</Text>
-        <View className=" flex-row grid gap-4">
-          <View className="w-[161px] h-[260px]">
-            <View className="h-[102px]">
-              <Image
-                source={require("../../../assets/img/top_rate_3.png")}
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: 10,
-                  // borderTopRightRadius: 5,
-                }}
-              />
-            </View>
-            <View className="flex-row items-center mt-1">
-              <Text className="font-[PlusSemiBold] text-[11px] mr-3 text-[#CD760F]">
-                Lorem ipsum
-              </Text>
-              <Ionicons name="checkmark-circle" size={11} color="black" />
-            </View>
-            <View>
-              <Text className="font-[PlusBold] text-[11px]">
-                Reservoir Engineering Principles
-              </Text>
-              <View className="flex-row mt-2">
-                <Text className="text-[8px]">Icons</Text>
-                <Text className="font[PlusSemiBold] text-[8px] mx-2">
-                  888 Student
-                </Text>
+
+        <FlatList
+          data={trendingData}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          //   keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <View className="w-[161px] h-[260] mx-3 mt-4">
+              <View className="w-[161px] h-[260px]">
+                <View className="h-[102px]">
+                  <Image
+                    source={require("../../../assets/img/top_rate_3.png")}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      borderRadius: 10,
+                      // borderTopRightRadius: 5,
+                    }}
+                  />
+                </View>
+                <View className="flex-row items-center mt-1">
+                  <Text className="font-[PlusSemiBold] text-[11px] mr-3 text-[#CD760F]">
+                    Lorem ipsum
+                  </Text>
+                  <Ionicons name="checkmark-circle" size={11} color="black" />
+                </View>
+                <View>
+                  <Text className="font-[PlusBold] text-[11px]">
+                    Reservoir Engineering Principles
+                  </Text>
+                  <View className="flex-row mt-2">
+                    <Text className="text-[8px]">Icons</Text>
+                    <Text className="font[PlusSemiBold] text-[8px] mx-2">
+                      888 Student
+                    </Text>
+                  </View>
+                </View>
+                <View className="flex-row items-center my-2 ">
+                  <Text className="font-[PlusBold] text-[13px]">N260.00</Text>
+                  <Text className="mx-2 line-through decoration-double font-[#Plusregular] text-[11px] text-[#545454]">
+                    N450.00
+                  </Text>
+                </View>
+                <TouchableOpacity className="w-[89] h-[28]  border justify-center items-center rounded-lg">
+                  <Text className="font-[PlusSemiBold] text-[12px] text-[#545454]">
+                    Add to cart
+                  </Text>
+                </TouchableOpacity>
               </View>
             </View>
-            <View className="flex-row items-center my-3 ">
-              <Text className="font-[PlusBold] text-[13px]">N260.00</Text>
-              <Text className="mx-2 line-through decoration-double font-[#Plusregular] text-[11px] text-[#545454]">
-                N450.00
-              </Text>
-            </View>
-            <TouchableOpacity className="w-[89] h-[28]  border justify-center items-center rounded-lg">
-              <Text className="font-[PlusSemiBold] text-[12px] text-[#545454]">
-                Add to cart
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View className="w-[161px] h-[260px] bg-red-500"></View>
-        </View>
+          )}
+        />
+
+        {/* <View className="w-[161px] h-[260px] bg-red-500"></View> */}
       </View>
     </View>
   );
-}
+};
 
 export default TrendingComp
